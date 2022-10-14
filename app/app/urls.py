d4ba74774c53458776a507ede715b4f97ba2cdde
CodeSysteme <co26teme@gmail.com>
2022-10-15 00:45:24 +0100

    Make a open api documentation

--- app/app/urls.py
+++ app/app/urls.py
@@ -9,7 +9,11 @@ BASE_API_URL = "api/v1"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(f"{BASE_API_URL}/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        f"{BASE_API_URL}/schema/",
+        SpectacularAPIView.as_view(),
+        name="api-schema",  # noqa
+    ),
     path(
         f"{BASE_API_URL}/swagger/",
         SpectacularSwaggerView.as_view(url_name="api-schema"),
